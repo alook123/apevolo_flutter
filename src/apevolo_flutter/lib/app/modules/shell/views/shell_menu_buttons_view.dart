9bb5e6f11749247936ca6274045cc62b0e9ada2c
alook <alook_6@live.com>
2024-07-25 13:42:30 +0800

    修改菜单主要的按钮的弹出菜单，以及布局widget

--- src/apevolo_flutter/lib/app/modules/shell/views/shell_menu_buttons_view.dart
+++ src/apevolo_flutter/lib/app/modules/shell/views/shell_menu_buttons_view.dart
@@ -14,10 +14,13 @@ class ShellMenuButtonsView extends GetView {
   late final bool _visible;
   @override
   Widget build(BuildContext context) {
-    final GlobalKey personMenuKey = GlobalKey();
     return Visibility(
       visible: _visible,
-      child: Row(
+      child: ButtonBar(
+        alignment: MainAxisAlignment.end,
+        buttonPadding: const EdgeInsets.all(0),
+        // buttonTextTheme: ButtonTextTheme.primary,
+        overflowButtonSpacing: 0,
         children: [
           IconButton(
             onPressed: () => Get.toNamed(Routes.HOME), //todo:修改成嵌套切换
@@ -27,43 +30,70 @@ class ShellMenuButtonsView extends GetView {
             onPressed: () {},
             icon: const Icon(Icons.message),
           ),
-          IconButton(
-            key: personMenuKey,
-            onPressed: () {
-              final RenderBox box =
-                  personMenuKey.currentContext!.findRenderObject() as RenderBox;
-              Offset position = box.localToGlobal(Offset.zero);
-              Size size = box.size;
-              showMenu(
-                context: context,
-                position: RelativeRect.fromLTRB(
-                  position.dx,
-                  position.dy + size.height,
-                  position.dx + size.width,
-                  position.dy,
-                ),
-                items: [
-                  PopupMenuItem(
-                    child: ListTile(
-                      leading: const Icon(Icons.account_box),
-                      title: const Text('个人资料'),
-                      onTap: () {},
-                    ),
-                  ),
-                  PopupMenuItem(
-                    child: ListTile(
-                      leading: const Icon(Icons.logout),
-                      title: const Text('注销'),
-                      onTap: () {
-                        controller.onLogout();
-                      },
-                    ),
-                  ),
-                ],
-                elevation: 8.0,
-              );
-            },
+          // IconButton(
+          //   key: personMenuKey,
+          //   onPressed: () {
+          //     final RenderBox box =
+          //         personMenuKey.currentContext!.findRenderObject() as RenderBox;
+          //     Offset position = box.localToGlobal(Offset.zero);
+          //     Size size = box.size;
+          //     showMenu(
+          //       context: context,
+          //       position: RelativeRect.fromLTRB(
+          //         position.dx,
+          //         position.dy + size.height,
+          //         position.dx + size.width,
+          //         position.dy,
+          //       ),
+          //       items: [
+          //         PopupMenuItem(
+          //           child: ListTile(
+          //             leading: const Icon(Icons.account_box),
+          //             title: const Text('个人资料'),
+          //             onTap: () {},
+          //           ),
+          //         ),
+          //         PopupMenuItem(
+          //           child: ListTile(
+          //             leading: const Icon(Icons.logout),
+          //             title: const Text('注销'),
+          //             onTap: () {
+          //               controller.onLogout();
+          //             },
+          //           ),
+          //         ),
+          //       ],
+          //       elevation: 8.0,
+          //     );
+          //   },
+          //   icon: const Icon(Icons.person),
+          // ),
+          PopupMenuButton(
+            tooltip: '个人中心',
             icon: const Icon(Icons.person),
+            onSelected: (value) {
+              // Handle the selected menu item
+              print('Selected: $value');
+            },
+            offset: const Offset(0, 40),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: ListTile(
+                  leading: const Icon(Icons.account_box),
+                  title: const Text('个人资料'),
+                  onTap: () {},
+                ),
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('注销'),
+                  onTap: () {
+                    controller.onLogout();
+                  },
+                ),
+              ),
+            ],
           ),
           IconButton(
             onPressed: () {},
