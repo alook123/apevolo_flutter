b24a3f12e8210049b14388d62efdc52f4c1c7729
alook <alook_6@live.com>
2025-06-24 22:50:06 +0800

    fix: 更新应用基础URL为HTTPS协议

--- lib/app/constants/apevolo_constants.dart
+++ lib/app/constants/apevolo_constants.dart
@@ -1,5 +1,5 @@
 /// This file is part of the Apevolo project.
 class ApevoloConstants {
   /// 应用名称
-  static const String baseURL = 'http://apevolo.api.alook.life';
+  static const String baseURL = 'https://apevolo-api.alook.life';
 }
